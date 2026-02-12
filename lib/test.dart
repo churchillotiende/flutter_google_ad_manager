258496afc98b386f5f98fa190732f4423253ad04
Churchill Onyango Otiende <churchill.otiende@naivas.co.ke>
2026-02-12 16:11:55 +0300

    Feat: updates the xml file to use the admob metadata

---
+++ lib/test.dart
@@ -0,0 +1,6 @@
+// ad unit ca-app-pub-8857590249051059/3117651021
+// AppId ca-app-pub-8857590249051059~6207781001
+// Sample metadata
+//  <meta-data
+// android:name="com.google.android.gms.ads.ca-app-pub-8857590249051059~6207781001"
+// android:value="ca-app-pub-8857590249051059/3117651021"/>
