7474fc2e7127c6b670d202f1f4623aadeeab267f
Churchill Onyango Otiende <churchill.otiende@naivas.co.ke>
2026-02-12 16:39:28 +0300

    chore: updates the pubspec to include google mobile ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_ad_manager/home_screen.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+
   runApp(const MyApp());
 }
 
